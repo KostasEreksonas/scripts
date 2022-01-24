2f9ba7e6814ac2e54466e6fe7b1290646d04ecf9
Kostas Ereksonas <k.ereksonas@gmail.com>
2022-01-24 14:41:12 +0200

    Added a couple of scripts made for Advent of Code 2021

---
+++ Python/Advent_of_Code/Day_1/count.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+count = 0
+values = open("input.txt")
+value_array = []
+
+for val in values:
+    value_array.append(val)
+
+for i in range(len(value_array)):
+    if value_array[i] > value_array[i-1]:
+        count += 1
+
+print("The result is:",count)
+values.close()
